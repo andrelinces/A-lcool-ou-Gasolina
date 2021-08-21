b31143f3af0a5a19413bda5170fc0a52fe7a3a9a
Andre Linces <andrelinces@yahoo.com.br>
2021-08-21 18:08:15 -0300

    Initial Commit

---
+++ Álcool ou GasolinaTests/A_lcool_ou_GasolinaTests.swift
@@ -0,0 +1,33 @@
+//
+//  A_lcool_ou_GasolinaTests.swift
+//  Álcool ou GasolinaTests
+//
+//  Created by Andre Linces on 21/08/21.
+//
+
+import XCTest
+@testable import A_lcool_ou_Gasolina
+
+class A_lcool_ou_GasolinaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
