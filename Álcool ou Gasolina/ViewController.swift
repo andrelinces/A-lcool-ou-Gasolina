cbd02bd21566c65844f42aec8cc0f027f19a365e
Andre Linces <andrelinces@yahoo.com.br>
2021-08-21 20:46:05 -0300

    Inserindo os códigos.

--- Álcool ou Gasolina/ViewController.swift
+++ Álcool ou Gasolina/ViewController.swift
@@ -8,6 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //criando estrutura da classe
+    @IBOutlet weak var resultadoLegenda: UILabel!
+    
+    @IBOutlet weak var precoAlcoolCampo: UITextField!
+    
+    @IBOutlet weak var precoGasolinaCampo: UITextField!
+    
+    //criando referência de ação do botão.
+    @IBAction func calcularCombustivel(_ sender: Any) {
+        
+        //criando constantes para recuperar os valores digitados, utilizando if para garantir os valores digitados.
+        if let precoAlcool = precoAlcoolCampo.text{
+            if let precoAlcool = precoAlcoolCampo.text{
+                
+                //Validar os valores digitados
+                
+            }
+            
+        }
+        
+    }
+    //Métodos para validar os valores digitados
+    func validarCampos(){
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
